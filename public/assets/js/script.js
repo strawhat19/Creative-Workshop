aa0b73e289b7c924a3dd17603bc60f6cdf5cf566
Rakib Ahmed <rakib987@gmail.com>
2023-11-11 13:38:52 -0500

    Base Template

---
+++ public/assets/js/script.js
@@ -0,0 +1,5 @@
+console.log(`Trello API Testing`);
+
+const copyrightYear = document.querySelector(`.year`);
+const copyrightYearValue = new Date().getFullYear();
+copyrightYear.innerHTML = copyrightYearValue;
