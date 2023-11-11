ed0274848531bc317ec890932fcb57fae5df23aa
Rakib Ahmed <rakib987@gmail.com>
2023-11-11 13:55:59 -0500

    Trello Form

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,13 @@
-console.log(`Trello API Testing`);
+const trelloForm = document.querySelector(`.trelloTicketForm`);
+let trelloTicketTitleField = document.querySelector(`.trelloTicketTitleField`);
+
+trelloForm.addEventListener(`submit`, (trelloFormSubmitEvent) => {
+    trelloFormSubmitEvent.preventDefault();
+
+    let ticketTitle = trelloTicketTitleField?.value;
+    console.log(`Trello API Testing`);
+    console.log(`Ticket Title`, ticketTitle);
+});
 
 const copyrightYear = document.querySelector(`.year`);
 const copyrightYearValue = new Date().getFullYear();
