3014690f3df522c6daa00defad700f82c8e6e48a
Rakib Ahmed <rakib987@gmail.com>
2023-11-17 01:31:41 -0500

    Adding Labels

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -33,6 +33,8 @@ const trelloTicketAttachmentField = document.querySelector(`.trelloTicketAttachm
 const trelloAPISecret = `ab406d5ee169689f283c6c45594de45a4d3091f6f13e2d6f6098adf325c1a9e9`;
 const trelloTicketDescriptionField = document.querySelector(`.trelloTicketDescriptionField`);
 const trelloAPIToken = `ATTA66b1ce6ba14d4eceb263827501181da8062d575eafa5510d055c2c246f56eede75D5CE72`;
+const paperclipIcon = `<svg title="Attachments" width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.6426 17.9647C10.1123 19.46 7.62736 19.4606 6.10092 17.9691C4.57505 16.478 4.57769 14.0467 6.10253 12.5566L13.2505 5.57184C14.1476 4.6952 15.5861 4.69251 16.4832 5.56921C17.3763 6.44184 17.3778 7.85135 16.4869 8.72199L9.78361 15.2722C9.53288 15.5172 9.12807 15.5163 8.86954 15.2636C8.61073 15.0107 8.60963 14.6158 8.86954 14.3618L15.0989 8.27463C15.4812 7.90109 15.4812 7.29546 15.0989 6.92192C14.7167 6.54838 14.0969 6.54838 13.7146 6.92192L7.48523 13.0091C6.45911 14.0118 6.46356 15.618 7.48523 16.6163C8.50674 17.6145 10.1511 17.6186 11.1679 16.6249L17.8712 10.0747C19.5274 8.45632 19.5244 5.83555 17.8676 4.2165C16.2047 2.59156 13.5266 2.59657 11.8662 4.21913L4.71822 11.2039C2.42951 13.4404 2.42555 17.083 4.71661 19.3218C7.00774 21.5606 10.7323 21.5597 13.0269 19.3174L19.7133 12.7837C20.0956 12.4101 20.0956 11.8045 19.7133 11.431C19.331 11.0574 18.7113 11.0574 18.329 11.431L11.6426 17.9647Z" fill="#9fadbc"></path></svg>`;
+const commentsIcon = `<svg title="Comments" width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 17H12.5L8.28037 20.4014C6.97772 21.4869 5 20.5606 5 18.865V16.1973C3.2066 15.1599 2 13.2208 2 11C2 7.68629 4.68629 5 8 5H16C19.3137 5 22 7.68629 22 11C22 14.3137 19.3137 17 16 17ZM16 7H8C5.79086 7 4 8.79086 4 11C4 12.8638 5.27477 14.4299 7 14.874V19L12 15H16C18.2091 15 20 13.2091 20 11C20 8.79086 18.2091 7 16 7Z" fill="#9fadbc"></path></svg>`;
 
 const getPercentage = (percent, number) => (percent / 100) * number;
 
@@ -141,6 +143,10 @@ const setBoardsData = (board) => {
             let cardLeft = document.createElement(`div`);
             let cardRight = document.createElement(`div`);
             let cardDeleteButton = document.createElement(`button`);
+            let cardFooter = document.createElement(`div`);
+            let footerLeft = document.createElement(`div`);
+            let footerRight = document.createElement(`div`);
+            let labelsElement = document.createElement(`div`);
 
             let firstStatusTier = getPercentage(25, lists.length);
             let secondStatusTier = getPercentage(50, lists.length);
@@ -152,10 +158,11 @@ const setBoardsData = (board) => {
                 status = `done`;
             }
 
-            cardElement.classList.add(`card`, `cardElement`);
+            cardElement.classList.add(`card`, `cardElement`, `relative`);
             cardElement.id = `card-${cardsIndex}-${card?.id}`;
 
-            cardTitle.classList.add(`cardTitle`, `flex`, `spaceBetween`);
+            cardTitle.classList.add(`cardTitle`, `flex`, `spaceBetween`, `gap5`);
+            cardTitle.title = `${card?.name} - ${card?.status}`
             cardLeft.classList.add(`cardLeft`, `textOverflow`);
             cardRight.classList.add(`cardRight`, `flex`, `gap10`, `alignCenter`, `justifyEnd`);
 
@@ -164,7 +171,7 @@ const setBoardsData = (board) => {
             cardStatus.innerHTML = card?.status;
 
             cardContent.classList.add(`cardContent`, `p15nb`);
-            cardContent.append(card?.description);
+            cardContent.innerHTML = card?.description?.replace(/\n/g, `<br>`);
             
             cardDeleteButton.id = `delete-card-${cardsIndex}-${card?.id}`;
             cardDeleteButton.classList.add(`cardDeleteButton`, `invertOnHover`, `p5`);
@@ -188,9 +195,25 @@ const setBoardsData = (board) => {
                 cardCover.append(cardCoverImage);
             }
 
+            cardFooter.classList.add(`cardFooter`, `absolute`, `flex`, `spaceBetween`, `gap5`, `alignCenter`, `w100`);
+            footerLeft.classList.add(`footerLeft`, `textOverflow`, `label`, `p15x`, `borderRadius`);
+
+            if (card?.labels?.length > 0) {
+                labelsElement.classList.add(`labelsElement`, `textOverflow`, `tflow100`);
+                labelsElement.innerHTML = [...new Set(card?.labels.map(lbl => lbl.name))].join(`, `);
+                footerLeft.append(labelsElement);
+            }
+
+            footerRight.classList.add(`footerRight`, `flex`, `gap5`, `alignCenter`, `justifyCenter`);
+            footerRight.innerHTML = `${card?.attachments > 0 ? `${paperclipIcon} ${card?.attachments}` : ``}${card?.comments > 0 ? `${commentsIcon} ${card?.comments}` : ``}`;
+
+            cardFooter.append(footerLeft);
+            cardFooter.append(footerRight);
+
             cardElement.append(cardTitle);
             if (card?.description != ``) cardElement.append(cardContent);
             if (card?.cover) cardElement.append(cardCover);
+            cardElement.append(cardFooter);
 
             cardsContainer.append(cardElement);
         })
@@ -230,10 +253,15 @@ const refreshBoard = async (boardID) => {
             statusLevel,
             id: crd?.id,
             name: crd?.name,
+            email: crd?.email,
             url: crd?.shortUrl,
             position: crd?.pos,
+            index: crd?.idShort,
             labels: crd?.labels,
             description: crd?.desc,
+            comments: crd?.badges.comments,
+            lastUpdated: crd?.dateLastActivity,
+            attachments: crd?.badges.attachments,
             ...(crd?.cover?.idAttachment != null && {
                 cover: `https://trello.com/1/cards/${crd?.id}/attachments/${crd?.cover?.idAttachment}/download/`,
             })
--- public/assets/js/utilities.js
+++ public/assets/js/utilities.js
@@ -1,90 +1,90 @@
 const getTrelloBoard = async (boardID) => {
-    try {
-        const trelloBoardResponse = await fetch(`https://api.trello.com/1/boards/${boardID}?key=${trelloAPIKey}&token=${trelloAPIToken}`);
-        if (!trelloBoardResponse.ok) console.log(`HTTP error! status: ${trelloBoardResponse.status}`);
-        const boardData = await trelloBoardResponse.json();
-        return boardData;
-    } catch (error) {
-        console.log(`Error fetching Board data: `, error);
-    }
+  try {
+    const trelloBoardResponse = await fetch(`https://api.trello.com/1/boards/${boardID}?key=${trelloAPIKey}&token=${trelloAPIToken}`);
+    if (!trelloBoardResponse.ok) console.log(`HTTP error! status: ${trelloBoardResponse.status}`);
+    const boardData = await trelloBoardResponse.json();
+    return boardData;
+  } catch (error) {
+    console.log(`Error fetching Board data: `, error);
+  }
 }
 
 const getTrelloBoardsLists = async (boardID) => {
-    try {
-        const trelloBoardsListResponse = await fetch(`https://api.trello.com/1/boards/${boardID}/lists?key=${trelloAPIKey}&token=${trelloAPIToken}`);
-        if (!trelloBoardsListResponse.ok) console.log(`HTTP error! status: ${trelloBoardsListResponse.status}`);
-        const boardsListsData = await trelloBoardsListResponse.json();
-        return boardsListsData;
-    } catch (error) {
-        console.log(`Error fetching Board Lists data: `, error);
-    }
+  try {
+    const trelloBoardsListResponse = await fetch(`https://api.trello.com/1/boards/${boardID}/lists?key=${trelloAPIKey}&token=${trelloAPIToken}`);
+    if (!trelloBoardsListResponse.ok) console.log(`HTTP error! status: ${trelloBoardsListResponse.status}`);
+    const boardsListsData = await trelloBoardsListResponse.json();
+    return boardsListsData;
+  } catch (error) {
+    console.log(`Error fetching Board Lists data: `, error);
+  }
 }
 
 const getTrelloCardsInBoard = async (boardID) => {
-    try {
-        const trelloCardsInBoardResponse = await fetch(`https://api.trello.com/1/boards/${boardID}/cards/all?key=${trelloAPIKey}&token=${trelloAPIToken}`);
-        if (!trelloCardsInBoardResponse.ok) console.log(`HTTP error! status: ${trelloCardsInBoardResponse.status}`);
-        const boardCardsData = await trelloCardsInBoardResponse.json();
-        return boardCardsData;
-    } catch (error) {
-        console.log(`Error fetching Board Cards data: `, error);
-    }
+  try {
+    const trelloCardsInBoardResponse = await fetch(`https://api.trello.com/1/boards/${boardID}/cards/all?key=${trelloAPIKey}&token=${trelloAPIToken}`);
+    if (!trelloCardsInBoardResponse.ok) console.log(`HTTP error! status: ${trelloCardsInBoardResponse.status}`);
+    const boardCardsData = await trelloCardsInBoardResponse.json();
+    return boardCardsData;
+  } catch (error) {
+    console.log(`Error fetching Board Cards data: `, error);
+  }
 }
 
 const createWebhook = async () => {
-    try {
-        const response = await fetch(`https://api.trello.com/1/webhooks/`, {
-            method: `POST`,
-            headers: {
-                'Content-Type': `application/json`
-            },
-            body: JSON.stringify({
-                key: trelloAPIKey,
-                idModel: testBoardID,
-                token: trelloAPIToken,
-                callbackURL: `https://smasherscape.vercel.app/api/webhook?type=board`,
-                description: `My Trello Webhook For Test Board`
-            })
-        });
-    
-        const latestBoardData = await response.json();
-        console.log(`Latest Board Data`, latestBoardData);
-        return latestBoardData;
-    } catch (error) {
-        if (error.response) {
-            console.log(`Response data`, await error.response.text());
-        } else {
-            console.log(`Error fetching Board Cards data: `, error);
-        }
+  try {
+    const response = await fetch(`https://api.trello.com/1/webhooks/`, {
+      method: `POST`,
+      headers: {
+          'Content-Type': `application/json`
+      },
+      body: JSON.stringify({
+          key: trelloAPIKey,
+          idModel: testBoardID,
+          token: trelloAPIToken,
+          callbackURL: `https://smasherscape.vercel.app/api/webhook?type=board`,
+          description: `My Trello Webhook For Test Board`
+      })
+    });
+
+    const latestBoardData = await response.json();
+    console.log(`Latest Board Data`, latestBoardData);
+    return latestBoardData;
+  } catch (error) {
+    if (error.response) {
+      console.log(`Response data`, await error.response.text());
+    } else {
+      console.log(`Error fetching Board Cards data: `, error);
     }
+  }
 }
 
 const isValid = (item) => {
-    if (typeof item == `string`) {
-      if (!item || item == `` || item.trim() == `` || item == undefined || item == null) {
-        return false;
-      } else {
-        return true;
-      }
-    } else if (typeof item == `number`) {
-      if (isNaN(item) || item == undefined || item == null) {
-        return false;
-      } else {
-        return true;
-      }
-    } else if (typeof item == `object` && item != undefined && item != null) {
-      if (Object.keys(item).length == 0 || item == undefined || item == null) {
-        return false;
-      } else {
-        return true;
-      }
-    } else if (Array.isArray(item) && item != undefined && item != null) {
-      return item.length > 0;
+  if (typeof item == `string`) {
+    if (!item || item == `` || item.trim() == `` || item == undefined || item == null) {
+      return false;
     } else {
-      if (item == undefined || item == null) {
-        return false;
-      } else {
-        return true;
-      }
+      return true;
     }
+  } else if (typeof item == `number`) {
+    if (isNaN(item) || item == undefined || item == null) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (typeof item == `object` && item != undefined && item != null) {
+    if (Object.keys(item).length == 0 || item == undefined || item == null) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (Array.isArray(item) && item != undefined && item != null) {
+    return item.length > 0;
+  } else {
+    if (item == undefined || item == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
